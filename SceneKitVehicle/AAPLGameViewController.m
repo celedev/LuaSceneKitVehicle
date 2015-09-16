e13d9e52f71b981e3bffcba1c3e8540317c467b8
Jean-Luc Jumpertz <jean-luc@celedev.eu>
2015-09-16 15:17:36 +0200

    Updated for CodeFlow 0.9.18

--- SceneKitVehicle/AAPLGameViewController.m
+++ SceneKitVehicle/AAPLGameViewController.m
@@ -136,7 +136,7 @@
     return YES;
 }
 
-- (NSUInteger)supportedInterfaceOrientations
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
 {
     return UIInterfaceOrientationMaskLandscape;
 }
